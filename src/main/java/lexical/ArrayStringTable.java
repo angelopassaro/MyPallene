ca85284fbb49413aa8308e0a35b2ceecf30a69e6
Angelo <angelopassaro1@gmail.com>
2020-01-20 15:33:08 +0100

    Added methods

--- src/main/java/lexical/ArrayStringTable.java
+++ src/main/java/lexical/ArrayStringTable.java
@@ -1,8 +1,9 @@
 package lexical;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
-public class ArrayStringTable implements StringTable  {
+public class ArrayStringTable implements StringTable {
 
     private ArrayList<String> lexicalSymbols;
 
@@ -12,7 +13,7 @@ public class ArrayStringTable implements StringTable  {
 
     @Override
     public boolean install(String lexeme) {
-        if(this.getAddress(lexeme) == -1) {
+        if (this.getAddress(lexeme) == -1) {
             return this.lexicalSymbols.add(lexeme);
         } else {
             return false;
@@ -27,6 +28,27 @@ public class ArrayStringTable implements StringTable  {
     @Override
     public String getLexeme(int address) {
         return this.lexicalSymbols.get(address);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.lexicalSymbols);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj == null) {
+            return false;
+        } else if (getClass() != obj.getClass()) {
+            return false;
+        } else {
+            final ArrayStringTable other = (ArrayStringTable) obj;
+            return this.lexicalSymbols.equals(other.lexicalSymbols);
+        }
     }
 
 }
--- src/main/java/lexical/StringTable.java
+++ src/main/java/lexical/StringTable.java
@@ -7,10 +7,29 @@ public interface StringTable {
      * analysis
      */
 
+
+    /**
+     * Add a new lexical symbol to table. I f the symbol is yet present it isn't added to table
+     *
+     * @param lexeme the symbol name
+     * @return true if no duplicates are found, false otherwise
+     */
     boolean install(String lexeme);
 
+    /**
+     * Get the addres for a specific lexeme
+     *
+     * @param lexeme the lexeme for searching
+     * @return the address of the lexeme
+     */
     int getAddress(String lexeme);
 
+    /**
+     * Get the lexeme associated with this address
+     *
+     * @param address the address to search
+     * @return the lexeme associated
+     */
     String getLexeme(int address);
 
 }
