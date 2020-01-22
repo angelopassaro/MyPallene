e5518028c47c9be9fc4cfbdfccd26dce732e1485
Angelo <angelopassaro1@gmail.com>
2020-01-22 18:02:06 +0100

    Implement errors

---
+++ src/main/java/error/StackErrorHandler.java
@@ -0,0 +1,37 @@
+package error;
+
+import syntax.AstNode;
+
+import java.util.Stack;
+
+public class StackErrorHandler implements ErrorHandler {
+
+    private final Stack<String> catchedErrors;
+
+    public StackErrorHandler() {
+        this.catchedErrors = new Stack<>();
+    }
+
+    @Override
+    public void reportError(String msg, AstNode node) {
+        StringBuilder errorBuilder = new StringBuilder();
+        errorBuilder.append(msg);
+        errorBuilder.append(" at ");
+        errorBuilder.append('(');
+        errorBuilder.append(node.getLeftLocation());
+        errorBuilder.append('-');
+        errorBuilder.append(node.getRightLocation());
+        errorBuilder.append(")");
+        this.catchedErrors.push(errorBuilder.toString());
+    }
+
+    @Override
+    public void logErrors() {
+        this.catchedErrors.forEach(System.out::println);
+    }
+
+    @Override
+    public boolean haveErrors() {
+        return this.catchedErrors.isEmpty();
+    }
+}
