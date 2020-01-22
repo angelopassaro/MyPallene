9929ca3f279fc2a42f6854e13643df9cee69838f
Angelo <angelopassaro1@gmail.com>
2020-01-22 18:11:44 +0100

    test scope check

--- src/main/java/cli/App.java
+++ src/main/java/cli/App.java
@@ -2,38 +2,48 @@ package cli;
 
 import core.Lexer;
 import core.Parser;
+import error.ErrorHandler;
+import error.StackErrorHandler;
 import java_cup.runtime.ComplexSymbolFactory;
+import lexical.ArrayStringTable;
+import lexical.StringTable;
 import org.w3c.dom.Document;
+import semantic.StackSymbolTable;
+import semantic.SymbolTable;
 import syntax.Program;
 import template.XMLTemplate;
 import visitor.ConcreteXMLVisitor;
+import visitor.ScopeCheckerVisitor;
 
 import java.io.File;
 import java.io.FileInputStream;
 
 class MyPalleneXML {
 
-    static Lexer lexer;
-    static Parser parser;
+    private static Lexer lexer;
+    private static Parser parser;
     private static ComplexSymbolFactory complexSymbolFactory = new ComplexSymbolFactory();
+    private static final String path = "/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/testfile/input1";
+    private static final ErrorHandler errorHandler = new StackErrorHandler();
+    private static final StringTable stringTable = new ArrayStringTable();
+    private static final SymbolTable symbolTable = new StackSymbolTable(stringTable);
 
     public static void main(String[] args) throws Exception {
 
-        lexer = new Lexer(complexSymbolFactory, new FileInputStream(new File(args[0])));
+        lexer = new Lexer(complexSymbolFactory, new FileInputStream(new File(path)));
         parser = new Parser(lexer, complexSymbolFactory);
 
         Program program = (Program) parser.parse().value;
+        ScopeCheckerVisitor scopeCheckerVisitor = new ScopeCheckerVisitor(errorHandler);
+        boolean test = program.accept(scopeCheckerVisitor, symbolTable);
+        System.out.println(test);
 
         XMLTemplate xmlTemplate = new XMLTemplate();
 
         Document xmlDocument = xmlTemplate.create().get();
-
         ConcreteXMLVisitor xmlVisitor = new ConcreteXMLVisitor();
-
         program.accept(xmlVisitor, xmlDocument);
-
-        xmlTemplate.write("/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es4_mpp/output/" + args[0].substring(args[0].lastIndexOf('/') + 1) + ".xml", xmlDocument);
-
+        xmlTemplate.write("/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/output/" + path.substring(path.lastIndexOf('/') + 1) + ".xml", xmlDocument);
 
     }
 }
