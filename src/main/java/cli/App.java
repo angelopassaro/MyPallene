7599b232b9ecb81e1cc378c6343073bac640ca3a
Angelo <angelopassaro1@gmail.com>
2020-02-06 18:03:52 +0100

    Fix grammar for array

--- src/main/java/cli/App.java
+++ src/main/java/cli/App.java
@@ -8,15 +8,11 @@ import java_cup.runtime.ComplexSymbolFactory;
 import lexical.ArrayStringTable;
 import lexical.StringTable;
 import org.w3c.dom.Document;
-import semantic.FakeSymbolTable;
 import semantic.StackSymbolTable;
-import semantic.SymbolTable;
 import syntax.Program;
+import template.CTemplate;
 import template.XMLTemplate;
-import visitor.ConcreteXMLVisitor;
-import visitor.PreScopeCheckerVisitor;
-import visitor.ScopeCheckerVisitor;
-import visitor.TypeCheckerVisitor;
+import visitor.*;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -26,10 +22,10 @@ class App {
     private static Lexer lexer;
     private static Parser parser;
     private static ComplexSymbolFactory complexSymbolFactory = new ComplexSymbolFactory();
-    private static final String path = "/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/testfile/input2";
+    private static final String path = "/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/testfile/input3.mypl";
     private static final ErrorHandler errorHandler = new StackErrorHandler();
     private static final StringTable stringTable = new ArrayStringTable();
-    private static final SymbolTable symbolTable = new StackSymbolTable(stringTable);
+    private static final StackSymbolTable symbolTable = new StackSymbolTable(stringTable);
 
     public static void main(String[] args) throws Exception {
 
@@ -40,23 +36,49 @@ class App {
         PreScopeCheckerVisitor preScopeCheckerVisitor = new PreScopeCheckerVisitor(errorHandler);
         TypeCheckerVisitor typeCheckerVisitor = new TypeCheckerVisitor(errorHandler);
 
+
         ScopeCheckerVisitor scopeCheckerVisitor = new ScopeCheckerVisitor(errorHandler);
         boolean prescope = program.accept(preScopeCheckerVisitor, symbolTable);
+        symbolTable.resetLevel();
+
         boolean scope = program.accept(scopeCheckerVisitor, symbolTable);
+        symbolTable.resetLevel();
 
-        program.accept(typeCheckerVisitor, new FakeSymbolTable((StackSymbolTable) symbolTable, stringTable));
+        program.accept(typeCheckerVisitor, symbolTable);
+        symbolTable.resetLevel();
 
-        System.out.println("PreScope: " + prescope + " scope: " + scope);
-        System.out.println(symbolTable.toString());
         if (errorHandler.haveErrors()) {
+            System.out.println("Errore durante la fase semantica");
             errorHandler.logErrors();
         }
 
-        XMLTemplate xmlTemplate = new XMLTemplate();
 
-        Document xmlDocument = xmlTemplate.create().get();
-        ConcreteXMLVisitor xmlVisitor = new ConcreteXMLVisitor();
-        program.accept(xmlVisitor, xmlDocument);
-        xmlTemplate.write("/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/output/" + path.substring(path.lastIndexOf('/') + 1) + ".xml", xmlDocument);
+        // System.out.println("PreScope: " + prescope + " scope: " + scope);
+        // System.out.println(symbolTable.toString());
+
+        String fileName = "/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/output/" + path.substring(path.lastIndexOf('/') + 1);
+
+        CTemplate template = new CTemplate();
+        String model = template.create().get();
+
+        PreCLangVisitor PreCLangVisitor = new PreCLangVisitor(model);
+        model = program.accept(PreCLangVisitor, symbolTable);
+        symbolTable.resetLevel();
+        template.write(fileName, model);
+
+        CLangVisitor cLangVisitor = new CLangVisitor(model);
+        model = program.accept(cLangVisitor, symbolTable);
+        symbolTable.resetLevel();
+        template.write(fileName, model);
+
+        if (args.length > 0 && args[0].equals("xml")) {
+            XMLTemplate xmlTemplate = new XMLTemplate();
+            Document xmlDocument = xmlTemplate.create().get();
+            ConcreteXMLVisitor xmlVisitor = new ConcreteXMLVisitor();
+            program.accept(xmlVisitor, xmlDocument);
+            xmlTemplate.write(fileName.replace("mypl", "xml"), xmlDocument);
+        }
+
+
     }
 }
--- src/main/java/cli/ParserTest.java
+++ src/main/java/cli/ParserTest.java
@@ -2,13 +2,11 @@ package cli;
 
 import core.Lexer;
 import core.Parser;
-import error.ErrorHandler;
-import error.StackErrorHandler;
+import core.ParserSym;
 import java_cup.runtime.ComplexSymbolFactory;
+import java_cup.runtime.Symbol;
 import lexical.ArrayStringTable;
 import lexical.StringTable;
-import semantic.StackSymbolTable;
-import semantic.SymbolTable;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,35 +16,32 @@ public class ParserTest {
     private static Lexer lexer;
     private static Parser parser;
     private static ComplexSymbolFactory complexSymbolFactory = new ComplexSymbolFactory();
-    private static final String path = "/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/testfile/input1";
-    private static final ErrorHandler errorHandler = new StackErrorHandler();
+    private static final String path = "/home/angelo/Documents/Universita/compilatori(GENNAIO)/esercizi/passaro_es5_scg/testfile/input3.mypl";
     private static final StringTable stringTable = new ArrayStringTable();
-    private static final SymbolTable symbolTable = new StackSymbolTable(stringTable);
 
     public static void main(String[] args) throws Exception {
 
         lexer = new Lexer(complexSymbolFactory, new FileInputStream(new File(path)), stringTable);
         parser = new Parser(lexer, complexSymbolFactory);
 
-/**
- //lexer OK
 
- Symbol token;
- try {
- while ((token = lexer.next_token()) != null) {
- if (token.sym == ParserSym.EOF) {
- break;
- }
- String toRet = "<" +
- ParserSym.terminalNames[token.sym] +
- (token.value == null ? ">" : (", " + token.value + ">"));
- System.out.println(toRet);
- }
- } catch (Exception e) {
- System.out.println("File parsing ended!!");
- }
- **/
-        //Parser not work with local
+        //lexer OK
+
+        Symbol token;
+        try {
+            while ((token = lexer.next_token()) != null) {
+                if (token.sym == ParserSym.EOF) {
+                    break;
+                }
+                String toRet = "<" +
+                        ParserSym.terminalNames[token.sym] +
+                        (token.value == null ? ">" : (", " + token.value + ">"));
+                System.out.println(toRet);
+            }
+        } catch (Exception e) {
+            System.out.println("File parsing ended!!");
+        }
+
         System.out.println(parser.parse().value);
     }
 
